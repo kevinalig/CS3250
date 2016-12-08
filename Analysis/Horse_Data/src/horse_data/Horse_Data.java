7a25bac694ce4573936f0fda0f43071bc484e47c
Kevin Alig <alig.15@wright.edu>
2016-12-08 05:55:49 -0500

    Cleaned up, hard coded filename.

--- Analysis/Horse_Data/src/horse_data/Horse_Data.java
+++ Analysis/Horse_Data/src/horse_data/Horse_Data.java
@@ -6,18 +6,10 @@
 package horse_data;
 
 import java.util.ArrayList;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Horse_Data {
